7aed3d18e8029b29610a6e454b8104d0b69bdefe
cljang <clayton.jang@gmail.com>
2022-06-17 13:53:23 -0700

    Built final app

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./Header";
 import Calculator from "./Calculator";
 import Footer from "./Footer";
@@ -5,16 +6,18 @@ import Footer from "./Footer";
 function App() {
  
   return (
-    <div className="wrapper">
-      <Header
-        title="React Calculator"
-      />
-      <Calculator />
-      <Footer 
-        year={new Date().getFullYear()}
-        author="Clayton Jang"
-      />
-    </div>
+    <BrowserRouter basename="/calculator">
+      <div className="wrapper">
+        <Header
+          title="React Calculator"
+        />
+        <Calculator />
+        <Footer
+          year={new Date().getFullYear()}
+          author="Clayton Jang"
+        />
+      </div>
+    </BrowserRouter>
   );
   
 }
